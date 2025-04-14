8c4d911c75b60dd31ad80afb54267a1d632049cd
Ivansallas <kiss_ivan@hotmail.com>
2025-04-14 16:53:20 -0300

    aborts

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const botoes = document.querySelectorAll('.botao');
+const abasConteudo = document.querySelectorAll(".aba-conteudo");
+
+// Mostrar a primeira aba por padrão
+abasConteudo[0].classList.add("ativo");
+
 for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () { 
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove('ativo');
-        }
-        botoes[i].classList.add('ativo');
+  botoes[i].onclick = function () {
+    // Remove a classe ativo de todos os botões
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
     }
-    
+    // Adiciona a classe ativo ao botão clicado
+    botoes[i].classList.add("ativo");
+
+    // Remove a classe ativo de todas as abas
+    for (let j = 0; j < abasConteudo.length; j++) {
+      abasConteudo[j].classList.remove("ativo");
+    }
+    // Adiciona a classe ativo à aba correspondente
+    abasConteudo[i].classList.add("ativo");
+  };
 }
