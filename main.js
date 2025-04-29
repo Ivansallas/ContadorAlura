7538e5e8c4954deaa10cca30970614b642833515
Ivansallas <kiss_ivan@hotmail.com>
2025-04-28 22:36:11 -0300

    10

--- main.js
+++ main.js
@@ -24,10 +24,14 @@ for (let i = 0; i < botoes.length; i++) {
   };
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2025-08-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-01-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-09-05T00:00:00");
+const tempoObjetivo4 = new Date("2025-10-05T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+// Adiciona os tempos objetivos em um lista para facilitar o loop
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
@@ -36,13 +40,27 @@ function calculaTempo(tempoObjetivo) {
   let minutos = Math.floor(segundos / 60);
   let horas = Math.floor(minutos / 60);
   let dias = Math.floor(horas / 24);
-
+  
   segundos %= 60;
   minutos %= 60;
   horas %= 24;
   
-  // Formata os valores para sempre ter dois dígitos com template literals 
-  return `${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos restantes até o evento.`;
+  if (tempoFinal > 0) {
+    // Formata os valores para sempre ter dois dígitos com template literals 
+    return `${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos restantes até o evento.`;
+  } else { 
+    return "O evento já encerrou!";
+    
+  }
 }
 
-    
+// Atualiza os contadores a cada segundo
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+atualizaCronometro(); // Atualiza imediatamente
+setInterval(atualizaCronometro, 1000); // Atualiza a cada segundo
+  
+  
